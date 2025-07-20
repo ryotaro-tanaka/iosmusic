5386e7027562670bfc6a3269739ac9ed7c497b31
ryotaro-tanaka <ryotaro_tanaka@outlook.com>
2025-07-20 20:12:58 +1000

    fix index.tsx because of removing import accidentlly

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,5 +1,10 @@
-import { StyleSheet } from 'react-native';
+import { Image } from 'expo-image';
+import { Platform, StyleSheet } from 'react-native';
 
+import { HelloWave } from '@/components/HelloWave';
+import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
